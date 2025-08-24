452c3f4478bd1c4de63e533c4116bc490d4d29d0
pdjdev <pdj5096@gmail.com>
2025-08-25 01:06:34 +0900

    fix: improve style of /onboarding/finish page

--- src/pages/Onboarding/OnboardingFinish.tsx
+++ src/pages/Onboarding/OnboardingFinish.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { completeOnboarding } from "../../services/preferencesManager";
 import ProgressBar from "../../components/ProgressBar";
+import OnboardingImage from "../../assets/icons/onboardingimg.svg";
 import './Onboarding.css';
 import './OnboardingFinish.css';
 
@@ -50,13 +51,22 @@ export default function OnboardingFinish() {
         <ProgressBar current={totalSteps} total={totalSteps} />
       </div>
       <div className="onboarding-finish-center-area">
-        <div className="onboarding-finish-message-box">
-          <span className="onboarding-finish-message-text">
-            {isCompleting ? '데이터 저장 중...' : '조사 완료!'}
-          </span>
-          <span className="onboarding-finish-message-sub-text">
-            {isCompleting ? '잠시만 기다려주세요' : '고생하셨어요!'}
-          </span>
+        <div className="onboarding-finish-content">
+          <div className="onboarding-finish-image-container">
+            <img 
+              src={OnboardingImage} 
+              alt="온보딩 완료" 
+              className="onboarding-finish-image"
+            />
+          </div>
+          <div className="onboarding-finish-message-box">
+            <span className="onboarding-finish-message-text">
+              {isCompleting ? '데이터 저장 중...' : '조사 완료!'}
+            </span>
+            <span className="onboarding-finish-message-sub-text">
+              {isCompleting ? '잠시만 기다려주세요' : '고생하셨어요!'}
+            </span>
+          </div>
         </div>
       </div>
     </div>
